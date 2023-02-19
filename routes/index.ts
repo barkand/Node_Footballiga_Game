38ec14560d53a6794a9186a400eeaf51284ac9fb
Hamid Barkand <30972715+barkand@users.noreply.github.com>
2023-02-19 23:51:30 +0330

    Remove Routers Get

--- routes/index.ts
+++ routes/index.ts
@@ -5,18 +5,6 @@ import gameController from "../controllers/team";
 const GameRouters = express.Router();
 GameRouters.use(express.json());
 
-GameRouters.get("/", (req: any, res: any) => {
-  res.send(
-    `
-      /team-card
-      /save-team
-      /check-team
-      /upload
-      /get-team-name
-    `
-  );
-});
-
 GameRouters.post(`/team-card`, gameController.teamCard);
 GameRouters.post(`/save-team`, gameController.saveTeam);
 GameRouters.post(`/check-team`, gameController.checkTeamName);
